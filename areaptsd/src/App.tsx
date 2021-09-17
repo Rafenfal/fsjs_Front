e6650a4b8abcb37acbccf021aebcaeb67a5d2b9c
Rafenfal <raphael.dusautoy@epitech.eu>
2021-09-17 13:53:07 +0200

    Changed ESLint config + Started navigation

---
+++ areaptsd/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Home from './pages/Home';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React</h1>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
